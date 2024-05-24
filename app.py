2d01418c34df3f364c3c78571a55cc178fa52ea7
Ben Ashael <114052702+Benashael@users.noreply.github.com>
2024-05-24 13:04:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import streamlit as st
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.model_selection import train_test_split
 
-st.set_page_config(page_title = "Sales Prediction using Python", page_icon = "🛒")
+st.set_page_config(page_title="Sales Prediction using Python", page_icon="🛒")
 
-st.title("Titanic Survival Prediction 🛒:")
+st.title("Sales Prediction 🛒")
 
 # Load the dataset
 def load_data():
@@ -21,8 +21,8 @@ st.header("Advertising Dataset")
 st.write(df.head())
 
 # Select features and target variable
-X = data[['TV', 'Radio', 'Newspaper']]
-y = data['Sales']
+X = df[['TV', 'Radio', 'Newspaper']]
+y = df['Sales']
 
 # Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
@@ -32,13 +32,13 @@ st.header("User Input Features")
 
 # Options
 def user_input_features():
-    TV = st.slider('TV (in thousands)', float(data.TV.min()), float(data.TV.max()), float(data.TV.mean()))
-    Radio = st.slider('Radio (in thousands)', float(data.Radio.min()), float(data.Radio.max()), float(data.Radio.mean()))
-    Newspaper = st.slider('Newspaper (in thousands)', float(data.Newspaper.min()), float(data.Newspaper.max()), float(data.Newspaper.mean()))
+    TV = st.slider('TV (in thousands)', float(df.TV.min()), float(df.TV.max()), float(df.TV.mean()))
+    Radio = st.slider('Radio (in thousands)', float(df.Radio.min()), float(df.Radio.max()), float(df.Radio.mean()))
+    Newspaper = st.slider('Newspaper (in thousands)', float(df.Newspaper.min()), float(df.Newspaper.max()), float(df.Newspaper.mean()))
     return {'TV': TV, 'Radio': Radio, 'Newspaper': Newspaper}
-                                                                                                                      
+
 input_df = pd.DataFrame(user_input_features(), index=[0])
-                                                                                                                    
+
 model_type = st.selectbox("Select Model", ["Random Forest", "Linear Regression", "K - Nearest Neighbors"])
 
 # Model
@@ -48,9 +48,8 @@ elif model_type == "Linear Regression":
     model = LinearRegression()
 elif model_type == "K - Nearest Neighbors":
     model = KNeighborsRegressor()
-else:
-    return None
 
+# Fit the model
 model.fit(X_train, y_train)
 
 # Prediction
